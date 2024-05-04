49cc6687847f9b863ec2c491971cfe5eb6e6d71b
webdev-mostofa <mdmostofaa24@gmail.com>
2024-05-04 12:38:35 +0600

    project done

---
+++ src/providers/AuthProvider.jsx
@@ -0,0 +1,55 @@
+import { createContext, useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+import auth from "../firebase/firebase.init";
+export const AuthContext = createContext(null);
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  console.log(user);
+  const [loading, setLoading] = useState(true);
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  const loginUser = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const updateInfo = (name) => {
+    setLoading(true);
+    return updateProfile(auth.currentUser, {
+      displayName: name,
+    });
+  };
+
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  const authInfo = { createUser, loginUser, user, updateInfo, loading, logOut };
+
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
+AuthProvider.propTypes = {
+  children: PropTypes.node,
+};
---
+++ src/routes/Routes.jsx
@@ -0,0 +1,41 @@
+import { createBrowserRouter } from "react-router-dom";
+import Root from "../layout/Root";
+import Home from "../pages/Home/Home";
+import ErrorPage from "../pages/ErrorPage/ErrorPage";
+import Login from "../pages/Login/Login";
+import SignUp from "../pages/SignUp/SignUp";
+import CheckOut from "../pages/CheckOut/CheckOut";
+import PrivateRoute from "./PrivateRoute/PrivateRoute";
+import Bookings from "../pages/Bookings/Bookings";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    errorElement: <ErrorPage></ErrorPage>,
+    element: <Root></Root>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>
+      },
+      {
+        path: "/signup",
+        element: <SignUp></SignUp>
+      },
+      {
+        path: "/checkout/:id",
+        element: <PrivateRoute><CheckOut></CheckOut></PrivateRoute>,
+        loader: ({params})=>fetch(`http://localhost:5000/services/${params.id}`)
+      },
+      {
+        path: "/bookings",
+        element: <PrivateRoute><Bookings></Bookings></PrivateRoute>
+      }
+    ],
+  },
+]);
+export default router;
